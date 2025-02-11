e3cd9912c187b35bfe977daef6e08661ab9f60ed
Mark Lisicic <mark.lisicic@agiledrop.com>
2025-02-11 17:49:45 +0100

    Refactor JS for dark mode

--- amd/src/darkmode.js
+++ amd/src/darkmode.js
@@ -21,74 +21,73 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-define([], function() {
-    var init = function() {
-        const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
-        const darkThemeToggle = document.querySelectorAll(".darktheme-checkbox");
+import {setUserPreference} from 'core_user/repository';
 
-        const setTheme = function(theme) {
-            document.documentElement.setAttribute('data-bs-theme', theme);
-        };
+const init = () => {
+    const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
+    const darkThemeToggle = document.querySelectorAll(".darktheme-checkbox");
 
-        const updateCheckboxes = function(checked) {
-            darkThemeToggle.forEach(function(checkbox) {
-                checkbox.checked = checked;
-            });
-        };
+    const setTheme = function(theme) {
+        document.documentElement.setAttribute('data-bs-theme', theme);
+    };
 
-        const initializeTheme = function() {
-            const storedDarkThemeSetting = getCookie("darkThemeEnabled");
-
-            if (storedDarkThemeSetting === null) {
-                setCookie("darkThemeEnabled", darkThemeMq.matches ? "1" : "0");
-                updateCheckboxes(darkThemeMq.matches);
-                setTheme(darkThemeMq.matches ? 'dark' : 'light');
-            } else {
-                const isDarkTheme = storedDarkThemeSetting === "1";
-                updateCheckboxes(isDarkTheme);
-                setTheme(isDarkTheme ? 'dark' : 'light');
-            }
-        };
-
-        const handleCheckboxChange = function(e) {
-            const isDarkThemeEnabled = e.target.checked;
-
-            setTheme(isDarkThemeEnabled ? 'dark' : 'light');
-            setCookie("darkThemeEnabled", isDarkThemeEnabled ? "1" : "0");
-            updateCheckboxes(isDarkThemeEnabled);
-        };
-
+    const updateCheckboxes = function(checked) {
         darkThemeToggle.forEach(function(checkbox) {
-            checkbox.addEventListener("change", handleCheckboxChange);
+            checkbox.checked = checked;
         });
+    };
 
-        initializeTheme();
+    const initializeTheme = function() {
+        const storedDarkThemeSetting = getCookie("darkThemeEnabled");
 
-        /**
-         * Retrieves the value of a cookie by name.
-         * @param {string} name - The name of the cookie.
-         * @returns {string|null} - The value of the cookie or null if not found.
-         */
-        function getCookie(name) {
-            const value = `; ${document.cookie}`;
-            const parts = value.split(`; ${name}=`);
-            if (parts.length === 2) {
-                return parts.pop().split(';').shift();
-            }
-            return null;
-        }
-
-        /**
-         * Sets a cookie with the given name and value.
-         * @param {string} name - The name of the cookie.
-         * @param {string} value - The value to set for the cookie.
-         */
-        function setCookie(name, value) {
-            document.cookie = `${name}=${value}; path=/; SameSite=None; Secure`;
+        if (storedDarkThemeSetting === null) {
+            setCookie("darkThemeEnabled", darkThemeMq.matches ? "1" : "0");
+            updateCheckboxes(darkThemeMq.matches);
+            setTheme(darkThemeMq.matches ? 'dark' : 'light');
+        } else {
+            const isDarkTheme = storedDarkThemeSetting === "1";
+            updateCheckboxes(isDarkTheme);
+            setTheme(isDarkTheme ? 'dark' : 'light');
         }
     };
 
-    return {
-        init: init
+    const handleCheckboxChange = function(e) {
+        const isDarkThemeEnabled = e.target.checked;
+
+        setTheme(isDarkThemeEnabled ? 'dark' : 'light');
+        setCookie("darkThemeEnabled", isDarkThemeEnabled ? "1" : "0");
+        updateCheckboxes(isDarkThemeEnabled);
     };
-});
+
+    darkThemeToggle.forEach(function(checkbox) {
+        checkbox.addEventListener("change", handleCheckboxChange);
+    });
+
+    initializeTheme();
+
+    /**
+     * Retrieves the value of a cookie by name.
+     * @param {string} name - The name of the cookie.
+     * @returns {string|null} - The value of the cookie or null if not found.
+     */
+    function getCookie(name) {
+        const value = `; ${document.cookie}`;
+        const parts = value.split(`; ${name}=`);
+        if (parts.length === 2) {
+            return parts.pop().split(';').shift();
+        }
+        return null;
+    }
+
+    /**
+     * Sets a cookie with the given name and value.
+     * @param {string} name - The name of the cookie.
+     * @param {string} value - The value to set for the cookie.
+     */
+    function setCookie(name, value) {
+        document.cookie = `${name}=${value}; path=/; SameSite=None; Secure`;
+        setUserPreference('theme-dark-mode', value === "0" ? "light" : "dark");
+    }
+};
+
+init();
