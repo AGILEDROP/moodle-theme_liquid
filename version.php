e2e95dc1ad522f769e52791c5253aba3aea3a856
Mark Lisicic <mark.lisicic@agiledrop.com>
2025-02-12 17:30:56 +0100

    Raise release version

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'theme_liquid';
 $plugin->version = '2025021200';
-$plugin->release = '1.0.0';
+$plugin->release = '1.0.1';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->requires = '2023100400';
 $plugin->dependencies = [
